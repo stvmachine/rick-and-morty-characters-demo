41534a5f0007e0462eb6534428f9a32210c4e025
stvmachine <esteban.valenzuela@github-stvmachine.com>
2025-11-09 23:13:01 +1100

    feat: setup Apollo Client with GraphQL endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import './index.css'
 import App from './App.tsx'
 
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache(),
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
